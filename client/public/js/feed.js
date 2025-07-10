ee8e8e1db77d6a531fc3ff8981fa34e153ac0f24
Jeremy Torres <jt8675@students.necsd.net>
2025-07-10 13:49:57 -0400

    jhgdsdfghj

---
+++ client/public/js/feed.js
@@ -0,0 +1,18 @@
+function checkKeyPress(e) {
+  if (e.keyCode == 32) {
+      e.preventDefault();
+    goToLocation('/feed');
+  }
+}
+window.onload = function() {
+  document.body.setAttribute("tabindex", "0");
+  document.body.focus();
+  document.body.addEventListener("keyup", checkKeyPress);
+  const clickTarget = document.getElementById("fade_text");
+  if(clickTarget) {
+    clickTarget.style.cursor = "pointer";
+    clickTarget.addEventListener("click" , () => {
+      goToLocation("/feed");
+    });
+  }
+};
--- client/public/js/index.js
+++ client/public/js/index.js
@@ -1,4 +1,6 @@
-
+function gotolocation(path){
+  window.locationref= path;
+}
 
 
 /*
@@ -9,3 +11,22 @@ function checkKeyPress(e) {
     goToLocation('/feed');
   }
 }
+window.onload = 
+function checkKeyPress(e) {
+  if (e.keyCode == 32) {
+      e.preventDefault();
+    goToLocation('/feed');
+  }
+}
+window.onload = function() {
+  document.body.setAttribute("tabindex", "0");
+  document.body.focus();
+  document.body.addEventListener("keyup", checkKeyPress);
+  const clickTarget = document.getElementById("fade_text");
+  if(clickTarget) {
+    clickTarget.style.cursor = "pointer";
+    clickTarget.addEventListener("click" , () => {
+      goToLocation("/feed");
+    });
+  }
+};
